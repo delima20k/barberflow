5b25ae8f3cdd4da7ffa7b391b7d33b55a599b59b
Alan Lima <delima20k@gmail.com>
2026-04-30 13:17:32 -0300

    feat(home): H2+H3 stories dentro do #app-header com background-image e carousel -1rem

--- apps/cliente/sw.js
+++ apps/cliente/sw.js
@@ -5,7 +5,7 @@
 // =============================================================
 class SWCliente {
 
-  static #CACHE_NAME = 'barberflow-cliente-v114';
+  static #CACHE_NAME = 'barberflow-cliente-v115';
 
   static #ASSETS = [
     '/manifest.json',
