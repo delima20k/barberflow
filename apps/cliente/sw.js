02def06ad91537239267779915f8b86d36786ff3
Alan Lima <delima20k@gmail.com>
2026-04-30 13:11:23 -0300

    style(mb): stories-carousel-wrap -2rem acima do hero header

--- apps/cliente/sw.js
+++ apps/cliente/sw.js
@@ -5,7 +5,7 @@
 // =============================================================
 class SWCliente {
 
-  static #CACHE_NAME = 'barberflow-cliente-v113';
+  static #CACHE_NAME = 'barberflow-cliente-v114';
 
   static #ASSETS = [
     '/manifest.json',
