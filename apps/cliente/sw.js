26dd3d2f41101e0a25c19bfc34c2385a1b4301d1
Alan Lima <delima20k@gmail.com>
2026-04-30 13:22:08 -0300

    style(mb): mb-hero-header full-width com margin-inline negativa

--- apps/cliente/sw.js
+++ apps/cliente/sw.js
@@ -5,7 +5,7 @@
 // =============================================================
 class SWCliente {
 
-  static #CACHE_NAME = 'barberflow-cliente-v115';
+  static #CACHE_NAME = 'barberflow-cliente-v116';
 
   static #ASSETS = [
     '/manifest.json',
