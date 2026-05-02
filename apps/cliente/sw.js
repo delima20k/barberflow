07f716beff8d5619a236f4db5b849f1dfa3d660b
Alan Lima <delima20k@gmail.com>
2026-05-02 09:28:40 -0300

    fix(css): texto branco e endereco no rodape em cards com imagem de fundo

--- apps/cliente/sw.js
+++ apps/cliente/sw.js
@@ -5,7 +5,7 @@
 // =============================================================
 class SWCliente {
 
-  static #CACHE_NAME = 'barberflow-cliente-v133';
+  static #CACHE_NAME = 'barberflow-cliente-v134';
 
   static #ASSETS = [
     '/manifest.json',
